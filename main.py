1a96bb896e197ff7cf587f5ee4ae690aff4514fc
WTiebebedigha22 <106900112+WTiebebedigha22@users.noreply.github.com>
2024-09-21 18:24:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,4 +24,33 @@ class Rectangle:
     return (self.width ** 2 + self.height ** 2) ** .5
 
   def get_picture(self):
-    if self.width
+    if self.width > 50 or self.height > 50:
+      return "Too big for picture."
+    rectangle = ("*" * self.width + "\n") * self.height
+    return rectangle
+
+  def get amount inside(self.shape):
+    max_width = self.width // shape.width
+    max_height = self.height // shape.height
+    return max_width * max_height
+
+
+class Square((Rectangle):
+
+  def __init__(self, length):
+    super().__init__(length, length)
+
+  def __str__(self):
+    return "Square(side=" + str(self.width) + ")"
+
+  def set_side(self, side):
+    self.width = side
+    self.height = side
+
+  def set_width(self, side):
+    self.width = side
+    self.height = side
+    
+  def set_height(self, side):
+    self.width = side
+    self.height = side
