09659550b6c285434e5ae92aca2b01cf650dc5a9
WTiebebedigha22 <106900112+WTiebebedigha22@users.noreply.github.com>
2024-09-21 15:57:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+class Rectangle:
 
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+
+  def __str__(self):
+    return "Rectangle(width=" + str(self.width) + \
+            ", height=" + str(self.height) + ")"
